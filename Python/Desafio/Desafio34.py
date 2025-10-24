a0af5979f4dc5116617c92dd3d5ac2e7dace5c80
Antonio Augusto <antonio.augusto@valmig.local>
2025-10-24 15:54:35 -0300

    Aplicando cores
    Colocando cor no if do desafio 34

--- Python/Desafio/Desafio34.py
+++ Python/Desafio/Desafio34.py
@@ -1,7 +1,7 @@
 '''Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento.
 Para salários superiores a R$1250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.'''
 
-salario = float(input('Digíte o valor atual de seu salário em R$: ').replace(',','.'))
+salario = float(input('\033[34mDigíte o valor atual de seu salário em R$: \033[m').replace(',','.'))
 
 if salario > 1250:
     salario = salario * 1.10
