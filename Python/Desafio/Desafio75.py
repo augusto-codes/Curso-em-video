8c9a6daea34a0457095c3c922eed4dce0018bc48
Antonio Augusto Ferreira Neto <antonioafn2212@gmail.com>
2026-09-08 21:48:32 -0300

    Desafio 75 ao 76
    76 Apenas começado

---
+++ Python/Desafio/Desafio75.py
@@ -0,0 +1,21 @@
+"""Análise de Dados em uma Tupla: Ler 4 valores pelo teclado e guardá-los em uma tupla. Mostrar:
+quantas vezes apareceu o valor 9, em que posição foi digitado o primeiro valor 3 e quais foram os
+números pares."""
+
+num1 = int(input("Digíte o primeiro valor: "))
+num2 = int(input("Digíte o segundo valor: "))
+num3 = int(input("Digíte o terceiro valor: "))
+num4 = int(input("Digíte o quarto valor: "))
+tupla_num = (num1, num2, num3, num4)
+
+print(f'{tupla_num}')
+print(f"O número 9 aparece {tupla_num.count(9)} vezes")
+if 3 in tupla_num:
+    print(f"O primeiro valor 3 aparece na {tupla_num.index(3)+1}ª posição ")
+else:
+    print('Não a número 3 na tupla')
+    
+for num in tupla_num:
+    resto = num % 2
+    if resto == 0:
+        print(f'O número {num} é par!!')
---
+++ Python/Desafio/Desafio76.py
@@ -0,0 +1,8 @@
+"""Lista de Preços com Tupla: Criar uma única tupla que contenha nomes de produtos e seus
+respectivos preços de forma sequencial. Mostrar uma listagem de preços organizada em formato
+tabular."""
+
+listagem = ('Lápis', 1.75, 'Borracha', 2, 'Caderno', 15.9, 'Estojo', 25, 'tranferidor', 4.2, 'Compasso', 9.99, 'Mochila', 120.32, 'Canetas', 22.3, 'Livro', 34.9)
+print('-'*40)
+print('         Listagem de preço       ')
+print('-'*40)
